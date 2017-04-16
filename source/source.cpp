20655da11ed8e3dea8d68bc80dd9d90fa57a82ef
darkjacky <jackykoning@zonnet.nl>
2017-04-16 14:50:35 +0200

    why can I not just release something that works the first time

--- source/source.cpp
+++ source/source.cpp
@@ -9,7 +9,8 @@ You are free to use and distribute this code without asking me and without credi
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	DEVMODE Gsyncfix;
 	EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &Gsyncfix); // get variables
-	ChangeDisplaySettings(&Gsyncfix, CDS_FULLSCREEN); // set fullscreen with current variables
+	Gsyncfix.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT | DM_DISPLAYFREQUENCY; // force ? I don't know
+	ChangeDisplaySettings(&Gsyncfix, CDS_FULLSCREEN | CDS_RESET); // set fullscreen with current variables
 	Sleep(50); // This is not even needed but doing it just in case
 	ChangeDisplaySettings(NULL, 0); // reset back to normal
 }
