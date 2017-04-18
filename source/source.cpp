971b45d331b6297113843f0ddcd63e155f07be60
darkjacky <jackykoning@zonnet.nl>
2017-04-18 02:19:23 +0200

    Fine!
    Due to the other script working 50% or less I changed it to this. Worked every time so far.

--- source/source.cpp
+++ source/source.cpp
@@ -6,11 +6,38 @@ You are free to use and distribute this code without asking me and without credi
 
 #include "windows.h"
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+void Fix(DISPLAY_DEVICE dd) {
 	DEVMODE Gsyncfix;
-	EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &Gsyncfix); // get variables
-	Gsyncfix.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT | DM_DISPLAYFREQUENCY; // force ? I don't know
-	ChangeDisplaySettings(&Gsyncfix, CDS_FULLSCREEN | CDS_RESET); // set fullscreen with current variables
-	Sleep(50); // This is not even needed but doing it just in case
-	ChangeDisplaySettings(NULL, 0); // reset back to normal
+	EnumDisplaySettings(dd.DeviceName, ENUM_CURRENT_SETTINGS, &Gsyncfix); // Fill Gsyncfix variables
+
+	{ // scope this
+		DEVMODE dm;
+		for (int iModeNum = 0; EnumDisplaySettings(dd.DeviceName, iModeNum, &dm) != 0; iModeNum++) { // Scan all possible display settings
+			if (Gsyncfix.dmPelsWidth == dm.dmPelsWidth && Gsyncfix.dmPelsHeight == dm.dmPelsHeight && Gsyncfix.dmDisplayFrequency != dm.dmDisplayFrequency) { // finding same resolution different frequency
+				Gsyncfix.dmDisplayFrequency = dm.dmDisplayFrequency; // This really forces the display to change settings
+				break;
+			}
+		}
+	}
+
+	Gsyncfix.dmFields = DM_DISPLAYFREQUENCY; // This should do
+	ChangeDisplaySettingsEx(dd.DeviceName, &Gsyncfix, NULL, CDS_FULLSCREEN | CDS_RESET, NULL); // Set application to fullscreen
+	ChangeDisplaySettingsEx(dd.DeviceName, NULL, NULL, 0, NULL); // reset back to normal
+}
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+	// Basic script scans videocard outputs and sees if there is a screen connected to it
+
+	DISPLAY_DEVICE VideoCardSlot;
+	VideoCardSlot.cb = sizeof(DISPLAY_DEVICE);
+
+	DWORD i = 0;
+	while (EnumDisplayDevices(NULL, i, &VideoCardSlot, 0)) {
+		DISPLAY_DEVICE Display = { 0 };
+		Display.cb = sizeof(DISPLAY_DEVICE);
+		if (EnumDisplayDevices(VideoCardSlot.DeviceName, 0, &Display, 0)) Fix(VideoCardSlot);
+		i++;
+	}
+
 }
